e3779a2e99c7bcc8acc0060b36a3e01c7953579a
Krishna Goyal <krishnagoyal599@gmail.com>
2026-09-07 15:50:15 +0530

    Time: 37 ms (10.48%) | Memory: 42.4 MB (56.83%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int pivotInteger(int n) {
+        
+        for (int x = 1; x <= n; x++) {
+            int leftSum = 0;
+            int rightSum = 0;
+            for (int i = 1; i <= x; i++) {
+                leftSum += i;
+            }
+ 
+            for (int j = x; j <= n; j++) {
+                rightSum += j;
+            }
+            if (leftSum == rightSum) {
+                return x;
+            }
+        }
+        return -1;
+    }
+}
