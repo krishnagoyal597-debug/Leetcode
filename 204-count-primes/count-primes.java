ecada7d07783b526f4a283ded823a25a02c6f32d
Krishna Goyal <krishnagoyal599@gmail.com>
2026-09-08 12:17:55 +0530

    Time: 718 ms (19.20%) | Memory: 79.9 MB (16.45%) - LeetSync

--- 204-count-primes/count-primes.java
+++ 204-count-primes/count-primes.java
@@ -1,16 +1,13 @@
 class Solution {
     public int countPrimes(int n) {
-        if (n <= 2) {
+
+        if (n <= 2){
             return 0;
         }
-
         boolean[] isPrime = new boolean[n];
-
-        // Initially assume every number from 2 is prime
         for (int i = 2; i < n; i++) {
             isPrime[i] = true;
         }
-
         for (int i = 2; i * i < n; i++) {
             if (isPrime[i]) {
                 for (int j = i * i; j < n; j += i) {
@@ -18,15 +15,14 @@ class Solution {
                 }
             }
         }
-
         int count = 0;
-
         for (int i = 2; i < n; i++) {
             if (isPrime[i]) {
                 count++;
             }
         }
-
         return count;
     }
 }
+
+
